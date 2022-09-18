4933c4ecd70ec5bd3d8b204dfbe6b354aa2e0609
EanPhillips8 <eanphillips8@gmail.com>
2022-09-18 12:54:03 -0400

    on

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+    int costSpring = 150;
+    int costSummer = 234;
+    int costFall = 129;
+    int costWinter = 320;
+    int totalCost = costSpring + costSummer + costFall + costWinter;
+
+        System.out.println("Your total yearly maintenance cost is " + totalCost + " dollars.");
+
     }
+
+
 }
